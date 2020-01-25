5733fec7750916f22f85d1fa42b682b8e84e221a
JKamue <mail@jkamue.de>
2020-01-25 18:42:52 +0100

    Improved aesthetics, level can now set window size and background color

--- dtos/GridDto.cs
+++ dtos/GridDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,20 @@ namespace Breakout.dtos
     class GridDto
     {
         public List<BlockDto> blocks;
+        public Color backColor;
         public int width;
         public int height;
+        public int pxWidth;
+        public int pxHeight;
 
-        public GridDto(List<BlockDto> blocks, int width, int height)
+        public GridDto(List<BlockDto> blocks, Color backColor, int width, int height, int pxWidth, int pxHeight)
         {
             this.blocks = blocks;
+            this.backColor = backColor;
             this.width = width;
-            this.height = height;   
+            this.height = height;
+            this.pxWidth = pxWidth;
+            this.pxHeight = pxHeight;
         }
     }
 }
