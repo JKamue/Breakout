73b379f08bda5f11c79a731f45500773d372d62f
JKamue <mail@jkamue.de>
2020-01-26 18:35:39 +0100

    Added player and player collision detection

--- classes/Ball.cs
+++ classes/Ball.cs
@@ -21,7 +21,9 @@ namespace Breakout.classes
         private GridController GridController;
         private Timer Clock;
 
-        public Ball(Form form, GridController grid, int startVelocity, double vecX, double vecY, int dirX, int dirY,
+        private Player Player;
+
+        public Ball(Form form, GridController grid, Player player, int startVelocity, double vecX, double vecY, int dirX, int dirY,
             int distanceTop, int distanceLeft,
             Size size, Color color, bool breakable, int speedAfterCollision)
             : base(distanceTop, distanceLeft, size, color, breakable, speedAfterCollision)
@@ -34,6 +36,7 @@ namespace Breakout.classes
 
             this.Form = form;
             this.GridController = grid;
+            this.Player = player;
 
             createTimer();
             Form.Controls.Add(this);
@@ -74,89 +77,83 @@ namespace Breakout.classes
 
         private void UpdatePosition(object sender, EventArgs e)
         {
-            Top += (int) VecY * Velocity * DirY;
-            Left += (int) VecX * Velocity * DirX;
+            Top += (int)Math.Round(VecY * Velocity * DirY);
+            Left += (int) Math.Round(VecX * Velocity * DirX);
+        }
+
+        private void CheckObjectForCollision(Block other)
+        {
+            var bounds = this.Bounds;
+            if (bounds.IntersectsWith(other.Bounds))
+            {
+                if (other.Broken)
+                {
+                    return;
+                }
+                if (other.Breakable)
+                {
+                    this.Form.Controls.Remove(other);
+                    other.Broken = true;
+                    if (this.Velocity < other.SpeedAfterCollision)
+                    {
+                        this.Velocity = other.SpeedAfterCollision;
+                    }
+                }
+
+                int dist1, dist2, dist3, dist4;
+                dist1 = dist2 = dist3 = dist4 = 100;
+
+                var rect1 = new Rectangle(bounds.X, bounds.Y, bounds.Width, 1);
+                var rect2 = new Rectangle(bounds.X + bounds.Width, bounds.Y, 1, bounds.Height);
+                var rect3 = new Rectangle(bounds.X, bounds.Y + bounds.Height, bounds.Width, 1);
+                var rect4 = new Rectangle(bounds.X, bounds.Y, 1, bounds.Height);
+
+                if (rect1.IntersectsWith(other.Bounds))
+                {
+                    dist1 = other.Bounds.Y + other.Bounds.Height - bounds.Y;
+                }
+
+                if (rect2.IntersectsWith(other.Bounds))
+                {
+                    dist2 = bounds.X + bounds.Width - other.Bounds.X;
+                }
+
+                if (rect3.IntersectsWith(other.Bounds))
+                {
+                    dist3 = bounds.Y + bounds.Height - other.Bounds.Y;
+                }
+
+                if (rect4.IntersectsWith(other.Bounds))
+                {
+                    dist4 = other.Bounds.X + other.Bounds.Width - bounds.X;
+                }
+
+                if (dist3 < dist2 && dist3 < dist1 && dist3 < dist4)
+                {
+                    DirY = -1;
+                } else if (dist2 <= dist3 && dist2 <= dist1 && dist2 < dist4)
+                {
+                    DirX = -1;
+                } else if (dist1 < dist3 && dist1 < dist2 && dist1 < dist4)
+                {
+                    DirY = 1;
+                } else if (dist4 <= dist3 && dist4 < dist2 && dist4 <= dist1)
+                {
+                    DirX = 1;
+                }
+            }
         }
 
         private void DetectCollisions(object sender, EventArgs e)
         {
             foreach (var other in this.GridController.Grid)
             {
-                if (other == null)
+                if (other != null)
                 {
-                    continue;
-                }
-                if (this.Bounds.IntersectsWith(other.Bounds))
-                {
-                    if (other.Broken)
-                    {
-                        continue;
-                    }
-                    if (other.Breakable)
-                    {
-                        this.Form.Controls.Remove(other);
-                        other.Broken = true;
-                    }
-
-
-                    // Calculate
-                    var dist1 = 100;
-                    var dist2 = 100;
-                    var dist3 = 100;
-                    var dist4 = 100;
-
-                    var bounds = this.Bounds;
-
-                    var rect1 = new Rectangle(bounds.X, bounds.Y, bounds.Width, 1);
-                    var rect2 = new Rectangle(bounds.X + bounds.Width, bounds.Y, 1, bounds.Height);
-                    var rect3 = new Rectangle(bounds.X, bounds.Y + bounds.Height, bounds.Width, 1);
-                    var rect4 = new Rectangle(bounds.X, bounds.Y, 1, bounds.Height);
-
-                    if (rect1.IntersectsWith(other.Bounds))
-                    {
-                        dist1 = other.Bounds.Y + other.Bounds.Height - bounds.Y;
-                    }
-
-                    if (rect2.IntersectsWith(other.Bounds))
-                    {
-                        dist2 = bounds.X + bounds.Width - other.Bounds.X;
-                    }
-
-                    if (rect3.IntersectsWith(other.Bounds))
-                    {
-                        dist3 = bounds.Y + bounds.Height - other.Bounds.Y;
-                    }
-
-                    if (rect4.IntersectsWith(other.Bounds))
-                    {
-                        dist4 = other.Bounds.X + other.Bounds.Width - bounds.X;
-                    }
-
-                    if (dist3 < dist2 && dist3 < dist1 && dist3 < dist4)
-                    {
-                        DirY = -1;
-                        break;
-                    }
-
-                    if (dist2 <= dist3 && dist2 <= dist1 && dist2 < dist4)
-                    {
-                        DirX = -1;
-                        break;
-                    }
-
-                    if (dist1 < dist3 && dist1 < dist2 && dist1 < dist4)
-                    {
-                        DirY = 1;
-                        break;
-                    }
-
-                    if (dist4 <= dist3 && dist4 < dist2 && dist4 <= dist1)
-                    {
-                        DirX = 1;
-                        break;
-                    }
+                    this.CheckObjectForCollision(other);
                 }
             }
+            this.CheckObjectForCollision(Player);
         }
     }
 }
---
+++ classes/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Breakout.classes
+{
+    class Player: Block
+    {
+        private int maxLeft;
+        private int maxRight;
+
+        public Player(Form form, int maxLeft, int maxRight, int distanceTop, int distanceLeft,
+            Size size, Color color, bool breakable, int speedAfterCollision)
+            : base(distanceTop, distanceLeft, size, color, breakable, speedAfterCollision)
+        {
+            form.KeyDown += new KeyEventHandler(this.KeyPressed);
+            form.Controls.Add(this);
+
+            form.MouseMove += new MouseEventHandler(this.syncWithMouse);
+
+            this.maxLeft = maxLeft;
+            this.maxRight = maxRight;
+        }
+
+        public void syncWithMouse(object sender, MouseEventArgs e)
+        {
+            if (e.X < maxRight && e.X > maxLeft)
+            {
+                Left = e.X;
+            }
+        }
+
+        public void KeyPressed(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left)
+            {
+                if (Left > maxLeft)
+                {
+                    Left -= 50;
+                }
+            } else if (e.KeyData == Keys.Right)
+            {
+                if (Left < maxRight)
+                {
+                    Left += 50;
+                }
+            }
+        }
+    }
+}
